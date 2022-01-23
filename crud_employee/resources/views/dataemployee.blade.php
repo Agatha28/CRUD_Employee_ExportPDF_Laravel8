9b143973cb7d4855b86e3b6cf60ca9052dd84f28
Agatha28 <agasantoso7@gmail.com>
2022-01-24 01:10:43 +0700

    change dataemployee

--- crud_employee/resources/views/dataemployee.blade.php
+++ crud_employee/resources/views/dataemployee.blade.php
@@ -19,9 +19,10 @@
                 <thead>
                   <tr>
                     <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Gender</th>
+                    <th scope="col">Phone Number</th>
+                    <th scope="col">Action</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -30,17 +31,6 @@
                     <td>Mark</td>
                     <td>Otto</td>
                     <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
                   </tr>
                 </tbody>
               </table>
