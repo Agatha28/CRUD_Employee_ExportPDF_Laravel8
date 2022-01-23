dd53c5a02134880c9a288a1d0803080ee5c46a2b
Agatha28 <agasantoso7@gmail.com>
2022-01-24 05:17:07 +0700

    change addemployee

--- crud_employee/resources/views/addemployee.blade.php
+++ crud_employee/resources/views/addemployee.blade.php
@@ -24,6 +24,13 @@
                       <label for="exampleInputEmail1" class="form-label">Name</label>
                       <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
+
+                    <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label">Gender</label>
+                      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+
+                   
                   
                     <button type="submit" class="btn btn-primary">Submit</button>
                   </form>
