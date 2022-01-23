36f29f23741a48c1c4af77c0bd0f9d665f088487
Agatha28 <agasantoso7@gmail.com>
2022-01-24 05:20:37 +0700

    change addemployee

--- crud_employee/resources/views/addemployee.blade.php
+++ crud_employee/resources/views/addemployee.blade.php
@@ -27,7 +27,12 @@
 
                     <div class="mb-3">
                       <label for="exampleInputEmail1" class="form-label">Gender</label>
-                      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                      <select class="form-select" aria-label="Default select example">
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </select>
                     </div>
 
                     <div class="mb-3">
