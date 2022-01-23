f05f9103354f256a63af0e579d62e77879539d66
Agatha28 <agasantoso7@gmail.com>
2022-01-24 05:10:55 +0700

    change addemployee

--- crud_employee/resources/views/addemployee.blade.php
+++ crud_employee/resources/views/addemployee.blade.php
@@ -16,7 +16,7 @@
     <div class="container">
 
         <div class="row justify-content-center">
-            <div class="col-6">
+            <div class="col-8">
               <div class="card">
                 <div class="card-body">
                   <form>
