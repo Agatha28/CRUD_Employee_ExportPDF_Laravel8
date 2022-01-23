93ceab31febb98c536e888719674d6bd6d9712c2
Agatha28 <agasantoso7@gmail.com>
2022-01-24 01:13:52 +0700

    change data employee

--- crud_employee/resources/views/dataemployee.blade.php
+++ crud_employee/resources/views/dataemployee.blade.php
@@ -31,6 +31,10 @@
                     <td>Mark</td>
                     <td>Otto</td>
                     <td>@mdo</td>
+                    <td>
+                        <button type="button" class="btn btn-danger">Danger</button>
+                        <button type="button" class="btn btn-warning">Warning</button>
+                    </td>
                   </tr>
                 </tbody>
               </table>
