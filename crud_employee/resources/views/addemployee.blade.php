f55ced82092c9e077af539645c85833f6bdfbf3c
Agatha28 <agasantoso7@gmail.com>
2022-01-26 06:04:16 +0700

    change addemployee view

--- crud_employee/resources/views/addemployee.blade.php
+++ crud_employee/resources/views/addemployee.blade.php
@@ -39,6 +39,11 @@
                       <label for="exampleInputEmail1" class="form-label">Phone Number</label>
                       <input type="number" name="notelpon" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
+
+                    <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label">Upload Photo</label>
+                      <input type="file" name="foto" class="form-control">
+                    </div>
                   
                     <button type="submit" class="btn btn-primary">Submit</button>
                   </form>
