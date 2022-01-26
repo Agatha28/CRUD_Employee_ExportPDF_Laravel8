7c2cd2b98c8af64cf0b3a719cc72778bcdd33bc8
Agatha28 <agasantoso7@gmail.com>
2022-01-27 00:42:41 +0700

    change dataemployee

--- crud_employee/resources/views/dataemployee.blade.php
+++ crud_employee/resources/views/dataemployee.blade.php
@@ -94,7 +94,7 @@
                   icon: "success",
                 });
               } else {
-                swal("Data is not deleted");
+                swal("Data is not deleted!");
               }
             });
     });
