928ad0c0c0abf075a2b4e371410ed770f1fb5d6f
Agatha28 <agasantoso7@gmail.com>
2022-01-28 14:44:07 +0700

    change data

--- crud_employee/resources/views/dataemployee.blade.php
+++ crud_employee/resources/views/dataemployee.blade.php
@@ -18,7 +18,7 @@
         <a href="/addemployee" class="btn btn-success">+Add Data</a>
         <div class="row g-3 align-items-center mt-2">
               <div class="col-auto">
-                <input type="search" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+                <input type="search" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline" placeholder="Search">
               </div>
             </div>
         <div class="row">
