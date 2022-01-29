7e1aaf3785259a51dc1871214639e7ddafa909d9
Agatha28 <agasantoso7@gmail.com>
2022-01-30 00:36:15 +0700

    adding package providers

--- crud_employee/config/app.php
+++ crud_employee/config/app.php
@@ -162,11 +162,12 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
+        
 
         /*
          * Package Service Providers...
          */
+         Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
